96aa963fc81dbb267b24c21c2ad5a3c75e88f402
joaquin <iglesiastj@gmail.com>
2017-08-05 14:30:20 +0200

    deviance analysis integrated in glm call

--- glm/src/stats_projects/core.clj
+++ glm/src/stats_projects/core.clj
@@ -4,8 +4,7 @@
             [incanter.stats :as istats]                        
             [incanter.optimize :as iopt]
             [incanter.excel :as xls]))
-;; TODO: test aic, test deviance analysis, rewrite deviance using bindings to
-;; avoid verbosity
+;; TODO: test aic, test deviance analysis
 ;; utility functions
 (defn apply-to-all
   "applies a function to all elements of a nx1 matrix (math vector)"
@@ -24,8 +23,7 @@
     0))
 
 (defn to-matrix
-  "turns vector into nx1 matrix. makes easier to use
-the optimizer."
+  "turns vector into nx1 matrix. makes it easier to use the optimizer."
   [a-seq]
   (if (i/matrix? a-seq)
     a-seq
@@ -58,7 +56,7 @@ the optimizer."
         y' (apply-to-all identity y)]
     (/ (i/sum (map helper y' p')) (i/nrow x))))
 
-;; multimethods for gradient and variance function
+;; multimethods for link gradient and variance function
 ; gradients
 (defmulti gradient :link)
 
@@ -97,37 +95,37 @@ the optimizer."
 ;; generalized linear model
 (defn glm [y x start & {:keys [family link]
                         :or {family log-binomial link logit}}]
-  (let [n (i/nrow x)
-        objective-function
-        (fn [theta] (family link y x theta)) ;define objective function
-        beta-hat
-        (:value (iopt/maximize objective-function start))
+  ;log-likelihood helper function, reduces verbosity
+  (defn log-lik [x theta]
+    (family link y x theta))
+  ;optimization helper function, reduces verbosity on deviance analysis
+  (defn max-lik [x start]
+    (let [objective (fn [theta] (log-lik x theta))]
+      (:value (iopt/maximize objective start))))
+  (let [n (i/nrow x) ;number of obvservations
+        ;xs for deviance analysis
+        [ones id] [(i/matrix (repeat n 1) 1) (i/identity-matrix n)]        
+        beta-hat (max-lik x start)
+        log-model (log-lik x beta-hat)
+        ;betas and log-like for deviance analysis
+        [beta-null beta-full] [(max-lik ones [0]) (max-lik id (repeat n 0))]
+        [log-null log-full] [(log-lik ones beta-null) (log-lik id beta-full)]
+        [null-deviance residual-deviance]
+        (mapv (fn [log] (i/mult 2 (- log-full log))) [log-null log-model])
+        ;computes variance
         parametrization {:x x :y y :beta beta-hat :link link
                          :family family} ;parametrization map for variance
         variance (i/diag (cov-matrix parametrization)) ;compute variance
+        ;akaike information criterion
         aic (* 2 (- n (family link y x beta-hat)))
         ]
-    {:beta beta-hat :variance variance :aic aic}))
-; deviance analysis
-(defn deviance-analysis [y x beta family link start]
-  (let [n (i/nrow x) ; number obvservations
-        [ones id] [(i/matrix 1 n) (i/identity-matrix n)]
-        null-objective
-        (fn [theta] (family link y ones))
-        full-objective
-        (fn [theta] (family link y id))
-        beta-null (:value (iopt/maximize null-objective start))
-        beta-full (:value (iopt/maximize full-objective start))
-        log-null (family link y ones beta-null)
-        log-full (family link y id beta-full)
-        log-model (family link y x beta)
-        ]
-    {:null-deviance (i/mult 2 (- log-full log-null))
-     :residual-deviance (i/mult 2 (- log-full log-model))}))
+    {:beta beta-hat :variance variance :aic aic
+     :null-deviance null-deviance :residual-deviance residual-deviance
+     }))
 
 ;; simulations and tests
 ; covariates
-(def x (istats/sample-mvn 100 :sigma (i/diag [1 2])))
+(def x (istats/sample-mvn 10 :sigma (i/diag [1 2])))
 ; parameters
 (def beta (i/matrix [0.5 -0.3] 1))
 ; latent variable
@@ -137,8 +135,6 @@ the optimizer."
 (def y (i/matrix (apply-to-all discretize (logit y-star)) 1))
 ; parametrization for variance multi
 (def parametrization {:x x :y y :beta beta :link logit :family log-binomial})
-(println (reduce i/plus (gradient parametrization)))
-(println (cov-matrix parametrization))
 ; test optimization
 (def optzed (glm y x [0 0]))
 (println optzed)
